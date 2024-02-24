d92b51a9a6121a041e7a0e715ca7486c564422d1
spekkio <24884679+SpekkioKilwala@users.noreply.github.com>
2024-02-24 20:22:40 +1100

    set up webpack development mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,15 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      title: 'Testing Zone',
       inject: 'head',
       scriptloading: 'defer',
     }),
